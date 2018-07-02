9a9e8870ec13796855d378cbbfc47478f956ea82
santiagonoguez <santiago@nul-unu.com>
2018-07-02 10:11:02 -0500

    remove all other components

--- html/components/widget-settings-ui-core/src/js/_settings.js
+++
@@ -1,1 +0,0 @@
-angular.module("risevision.widget.common", []);
--- html/components/widget-settings-ui-core/src/js/svc-gadgets-api.js
+++
@@ -1,4 +0,0 @@
-angular.module("risevision.widget.common")
-  .factory("gadgetsApi", ["$window", function ($window) {
-    return $window.gadgets;
-  }]);
