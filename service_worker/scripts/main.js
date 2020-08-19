cf20c5b55603ec77b2c7d709565c72397bd0c665
santiagonoguez <santiago.arriaga9@gmail.com>
2020-08-19 11:13:05 -0500

    quitar sitio anterior

--- service_worker/scripts/main.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    // Registration was successful
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
