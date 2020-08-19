a5f1ef7e7126bbaeac8b42e47a21b63d2af86950
santiagonoguez <santiago.arriaga9@gmail.com>
2020-08-19 11:14:48 -0500

    prueba blockstack en theheroloop

---
+++ precache-manifest.8ca365353939eba1af58b5d3fea70c59.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a2d856204372aaa6a0f2343a22f67e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f3b3a22109a04590007",
+    "url": "/static/css/2.adbfb4be.chunk.css"
+  },
+  {
+    "revision": "7af8a37f3f7eda89b0c7",
+    "url": "/static/css/main.c59f71a2.chunk.css"
+  },
+  {
+    "revision": "8f3b3a22109a04590007",
+    "url": "/static/js/2.098e7cf9.chunk.js"
+  },
+  {
+    "revision": "7e8661572dbf62ca66d7837115f59618",
+    "url": "/static/js/2.098e7cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af8a37f3f7eda89b0c7",
+    "url": "/static/js/main.4166d82b.chunk.js"
+  },
+  {
+    "revision": "0683e82edb858aa962d3",
+    "url": "/static/js/runtime-main.cda92cd5.js"
+  },
+  {
+    "revision": "e0b52af1693213c1e8f303b1bb1dac24",
+    "url": "/static/media/chat.e0b52af1.svg"
+  },
+  {
+    "revision": "71868cdad895fa218d9675c468ef96df",
+    "url": "/static/media/close.71868cda.svg"
+  },
+  {
+    "revision": "0801f37c051c9eaa50d1749e21bcc947",
+    "url": "/static/media/filter.0801f37c.svg"
+  },
+  {
+    "revision": "d464a1b6e3f12f875389ca21cbe771d3",
+    "url": "/static/media/food.d464a1b6.svg"
+  },
+  {
+    "revision": "fe00b2a5b6252318647ac381d0961107",
+    "url": "/static/media/food.fe00b2a5.svg"
+  },
+  {
+    "revision": "3a08f2fdcdbe61691eb44744e31d62b8",
+    "url": "/static/media/hallOfFame.3a08f2fd.svg"
+  },
+  {
+    "revision": "11dd6fa2333e2e9f0ed07efa9e718ab7",
+    "url": "/static/media/hamburger.11dd6fa2.svg"
+  },
+  {
+    "revision": "1d7eef2ee576f14629c8010dad91b9e9",
+    "url": "/static/media/hand-heart.1d7eef2e.svg"
+  },
+  {
+    "revision": "a93901f2a6ce3c79c645c10a84459881",
+    "url": "/static/media/hands_orange.a93901f2.svg"
+  },
+  {
+    "revision": "05bc12f7e063b21018c2fb239bc4880c",
+    "url": "/static/media/heroLog.05bc12f7.svg"
+  },
+  {
+    "revision": "071dc5a934a92017776e8c039e59a3bf",
+    "url": "/static/media/hero_log.071dc5a9.png"
+  },
+  {
+    "revision": "8e7eeb3ec899d673860e15b7bfc67d87",
+    "url": "/static/media/image.8e7eeb3e.png"
+  },
+  {
+    "revision": "6b925b00d706e49bdf1aca5754938fc7",
+    "url": "/static/media/lock-heart.6b925b00.svg"
+  },
+  {
+    "revision": "85cc08cc2abfa277d88e741f2e1d41a0",
+    "url": "/static/media/login-exp.85cc08cc.gif"
+  },
+  {
+    "revision": "64bba70f9f8cd8f268d234f3602437e5",
+    "url": "/static/media/logo.64bba70f.png"
+  },
+  {
+    "revision": "dc829d8655eb297619e0dcf554c456ba",
+    "url": "/static/media/looper.dc829d86.png"
+  },
+  {
+    "revision": "0c19b5fef59c0a451875e620ef7f4306",
+    "url": "/static/media/map.0c19b5fe.svg"
+  },
+  {
+    "revision": "d632bcd72944ceb402aabd7293b3312c",
+    "url": "/static/media/mytasks2medium.d632bcd7.png"
+  },
+  {
+    "revision": "28b2abfa7ac1464845e553013ff13e36",
+    "url": "/static/media/mytasks2top.28b2abfa.png"
+  },
+  {
+    "revision": "0d0bb8020e97dea60c6d2eacd94d2c3a",
+    "url": "/static/media/mytasksbottom.0d0bb802.png"
+  },
+  {
+    "revision": "b43b9ee875abf0dff8fe0ae63ce0f32e",
+    "url": "/static/media/mytasksmedium.b43b9ee8.png"
+  },
+  {
+    "revision": "12179124d5140ba333fff3c64e354d52",
+    "url": "/static/media/mytaskstop.12179124.png"
+  },
+  {
+    "revision": "b1c90cd00d38b650fcd1324df57f0b8e",
+    "url": "/static/media/mytasksweb1.b1c90cd0.png"
+  },
+  {
+    "revision": "16aced63e08117301e7968c033e29308",
+    "url": "/static/media/mytasksweb2.16aced63.png"
+  },
+  {
+    "revision": "a75f42456aaf40ac4f89275883e7f5dc",
+    "url": "/static/media/mytasksweb3.a75f4245.png"
+  },
+  {
+    "revision": "ecf719527a4ccb7bc306b794f7e8e051",
+    "url": "/static/media/mytasksweb4.ecf71952.png"
+  },
+  {
+    "revision": "5ade125168f01d355ac0db4d5aab753f",
+    "url": "/static/media/other.5ade1251.svg"
+  },
+  {
+    "revision": "c6a0d6a9fdc267c14b91fd617ef12230",
+    "url": "/static/media/other.c6a0d6a9.svg"
+  },
+  {
+    "revision": "23100ee5c46cd7d8c82a5ba6b97dfc71",
+    "url": "/static/media/people-volunteer.23100ee5.png"
+  },
+  {
+    "revision": "84fd93615ff2a742844b25384dc2c2e5",
+    "url": "/static/media/person-heart.84fd9361.svg"
+  },
+  {
+    "revision": "1d913c757d0c202dad0cfacfa519e6f7",
+    "url": "/static/media/pharmacy.1d913c75.svg"
+  },
+  {
+    "revision": "b0436753d803db69f58b3ba1085e9714",
+    "url": "/static/media/pharmacy.b0436753.svg"
+  },
+  {
+    "revision": "ed40f0b6d44f38927e5bbedcc7de6334",
+    "url": "/static/media/profile.ed40f0b6.svg"
+  },
+  {
+    "revision": "9bf182019be6cba2684f871c220c5ee0",
+    "url": "/static/media/profilepic.9bf18201.png"
+  },
+  {
+    "revision": "2394036caf185edcd3b0e923ce4dc05b",
+    "url": "/static/media/rateLooper.2394036c.svg"
+  },
+  {
+    "revision": "456e6b6d16934e5bc9929ec9760e7446",
+    "url": "/static/media/request.456e6b6d.gif"
+  },
+  {
+    "revision": "380050b6d093aaea2ef10b6810420fc1",
+    "url": "/static/media/task.380050b6.svg"
+  },
+  {
+    "revision": "4ec8ae71a73076bc498730465582c537",
+    "url": "/static/media/the-digital-volunteer-key-visual.4ec8ae71.png"
+  },
+  {
+    "revision": "1722908091fd0f5c6f74f74fd66e7367",
+    "url": "/static/media/travel.17229080.svg"
+  },
+  {
+    "revision": "6518d3e36a891ac65ee5ec65708f534c",
+    "url": "/static/media/travel.6518d3e3.svg"
+  },
+  {
+    "revision": "d628c280d63396b4de7f37efd3be17e8",
+    "url": "/static/media/volunteer.d628c280.png"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
